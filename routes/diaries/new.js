fc2a932c807b4f8e63ef56f32d4de4cec45ea53d
sifue <yoshimura@soichiro.org>
2017-07-15 20:47:17 +0900

    投稿をDBに保存するところまで実装

--- routes/diaries/new.js
+++ routes/diaries/new.js
@@ -2,9 +2,24 @@
 const express = require('express');
 const router = express.Router();
 const authenticationEnsurer = require('../authentication-ensurer');
+const Diary = require('../../models/diary');
+const moment = require('moment');
 
 router.get('/', authenticationEnsurer, (req, res, next) => {
   res.render('diaries/new', { user: req.user });
 });
 
+router.post('/', authenticationEnsurer, (req, res, next) => {
+  console.log(req.body); // TODO 予定と候補を保存する実装をする
+  const diaryId = moment(new Date()).format('YYYYMMDDHHmmssSSS');
+  Diary.create({
+    diaryId: diaryId,
+    title: req.body.title.slice(0, 255),
+    body: req.body.body,
+    userId: req.user.id,
+    isDeleted: false
+  }).then(() => {
+    res.redirect('/');
+  });
+});
 module.exports = router;
